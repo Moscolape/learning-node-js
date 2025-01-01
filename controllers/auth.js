8452abfd87cb43adcc3ff5c3c97f684a8ba8ae57
moscolape <chunkums088@gmail.com>
2025-01-01 13:12:50 +0100

    chore: integrate the use of nodemailer for sending mails

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,8 @@
+const transporter = require("../utils/email");
 const User = require("../models/user");
 const bcrypt = require("bcryptjs");
+const crypto = require("crypto");
+
 
 exports.getLogin = (req, res, next) => {
   console.log(req.session.isLoggedIn);
@@ -17,7 +20,7 @@ exports.getSignup = (req, res, next) => {
     path: "/signup",
     docTitle: "Signup",
     isAuthenticated: false,
-    errorMessage: ""
+    errorMessage: "",
   });
 };
 
@@ -126,5 +129,117 @@ exports.postLogout = (req, res, next) => {
   req.session.destroy((err) => {
     console.log(err);
     res.redirect("/login");
+  });
+};
+
+exports.getReset = (req, res, next) => {
+  res.render("auth/forgotpassword", {
+    path: "/forgotten-password",
+    docTitle: "Forgotten Password",
+    isAuthenticated: false,
+    errorMessage: "",
+  });
+};
+
+
+exports.getChangePassword = (req, res, next) => {
+  const { token } = req.params;
+  res.render("auth/resetpassword", {
+    path: "/reset-password",
+    docTitle: "Reset Password",
+    isAuthenticated: false,
+    errorMessage: "",
+    token,
+  });
+};
+
+
+exports.postChangePassword = (req, res, next) => {
+  const { token } = req.params;
+  const { password } = req.body;
+
+  User.findOne({
+    resetToken: token,
+    resetTokenExpiration: { $gt: Date.now() },
+  })
+    .then((user) => {
+      if (!user) {
+        return res.status(400).render("auth/resetpassword", {
+          path: "/reset-password",
+          docTitle: "Reset Password",
+          isAuthenticated: false,
+          errorMessage: "Token is invalid or has expired. Please try resetting again.",
+        });
+      }
+
+      return bcrypt
+        .hash(password, 12)
+        .then((hashedPassword) => {
+          user.password = hashedPassword;
+          user.resetToken = undefined;
+          user.resetTokenExpiration = undefined;
+          
+          return user.save();
+        })
+        .then(() => {
+          res.redirect("/login");
+        });
+    })
+    .catch((err) => {
+      console.error(err);
+      next(err);
+    });
+};
+
+
+exports.postReset = (req, res, next) => {
+  crypto.randomBytes(32, (err, buffer) => {
+    if (err) {
+      console.log(err);
+      return res.redirect("/");
+    }
+    const token = buffer.toString("hex");
+
+    User.findOne({ email: req.body.email })
+      .then((user) => {
+        if (!user) {
+          return res.status(404).render("auth/forgotpassword", {
+            path: "/forgotten-password",
+            docTitle: "Forgotten Password",
+            isAuthenticated: false,
+            errorMessage: "No account with this email exists!",
+          });
+        }
+
+        user.resetToken = token;
+        // @ts-ignore
+        user.resetTokenExpiration = Date.now() + 3600000;
+        return user.save();
+      })
+      .then((result) => {
+        if (!result) return;
+        
+        return transporter.sendMail({
+          to: req.body.email,
+          from: "chunkums088@gmail.com",
+          subject: "Password Reset",
+          html: `
+            <p>You requested a password reset.</p>
+            <p>Click this <a href="http://localhost:4000/reset-password/${token}">link</a> to reset your password.</p>
+          `,
+        });
+      })
+      .then(() => {
+        return res.status(400).render("auth/login", {
+          path: "/login",
+          docTitle: "Login",
+          errorMessage: "Password reset link has been successfully sent to your email address.",
+          isAuthenticated: false,
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+        next(err);
+      });
   });
 };
--- routes/auth.js
+++ routes/auth.js
@@ -8,6 +8,14 @@ router.get('/login', authController.getLogin);
 
 router.get('/signup', authController.getSignup);
 
+router.get('/forgotten-password', authController.getReset);
+
+router.get('/reset-password/:token', authController.getChangePassword);
+
+router.post('/reset-password/:token', authController.postChangePassword);
+
+router.post('/forgotten-password', authController.postReset);
+
 router.post('/login', authController.postLogin);
 
 router.post('/signup', authController.postSignup);
